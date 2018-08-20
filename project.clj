86af7507d806fa802e9ec12878a4f2661d44137f
Felipe <fpaolozze@gmail.com>
2018-08-20 20:19:29 -0300

    remove clj script and updating clojure version

--- project.clj
+++ project.clj
@@ -1,13 +1,12 @@
-(defproject cadastro-de-pessoa "0.4.0"
+(defproject br.com.vikingmakt/cadastro-de-pessoa "0.4.1"
   :description "Some utils for working with cpfs and cnpjs."
   :url "http://github.com/madstap/cadastro-de-pessoa"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.9.293"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]]
   :profiles
-  {:dev {:dependencies [[com.cemerick/piggieback "0.2.1"]
-                        [org.clojure/tools.nrepl "0.2.10"]]
+  {:dev {:dependencies [[com.cemerick/piggieback "0.2.2"]
+                        [org.clojure/tools.nrepl "0.2.13"]]
          :repl-options {:nrepl-middleware
                         [cemerick.piggieback/wrap-cljs-repl]}}}
   :plugins [[lein-cljsbuild "1.1.4"]]
